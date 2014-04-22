79bf6018dacda0db2097099a9a7e935f49c6a8cd
Fred Bricon <fbricon@gmail.com>
2014-04-22 11:30:18 +0200

    JBIDE-17153 : IPropertiesProvider implem for JBDS
    also removes JVM properties for discovery urls from jbdevstudio.ini
    
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ plugins/com.jboss.devstudio.core.central/src/com/jboss/devstudio/core/internal/properties/DevStudioPropertiesProvider.java
@@ -0,0 +1,32 @@
+/*************************************************************************************
+ * Copyright (c) 2014 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
+package com.jboss.devstudio.core.internal.properties;
+
+import org.jboss.tools.foundation.core.properties.internal.VersionPropertiesProvider;
+
+/**
+ * Properties Provider for JBoss Developer Studio
+ *
+ * @author Fred Bricon
+ * @since 8.0.0
+ */
+public class DevStudioPropertiesProvider extends VersionPropertiesProvider {
+
+  public DevStudioPropertiesProvider() {
+    super();
+  }
+
+
+  @Override
+  protected String getVersionBundleName() {
+    return "com.jboss.devstudio.core.internal.properties.currentversion";
+  }
+}
