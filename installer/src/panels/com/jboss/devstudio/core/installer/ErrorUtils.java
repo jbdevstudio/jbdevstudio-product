35e9d21691ae67d381e0e7b4d4e8907e850f324b
Denis Golovin <dgolovin@gmail.com>
2015-03-31 10:08:14 -0700

    [JBDS-3391] Installer error reporting in automated mode
    This fix redirects error messages to standard output in
    automated mode.

--- installer/src/panels/com/jboss/devstudio/core/installer/ErrorUtils.java
+++ installer/src/panels/com/jboss/devstudio/core/installer/ErrorUtils.java
@@ -21,43 +21,47 @@ import javax.swing.SwingConstants;
 public class ErrorUtils {
 	 
 	public static void showError(JFrame owner, ImageIcon image, String title, String message, String text){
-		final JDialog frame = new JDialog(owner,true);
-		frame.setTitle(title);
-		frame.setLayout(new BorderLayout());
-		//frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-		JLabel label = new JLabel(message,image, SwingConstants.LEFT);
-
-		label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		
-		JTextArea area = new JTextArea(text);
-		area.setEditable(false);
-		area.setWrapStyleWord(false);
-		area.setLineWrap(false);
-		
-		JScrollPane scroll = new JScrollPane(area);
-		scroll.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		
-		JButton ok = new JButton("OK");
-		ok.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				frame.dispose();
-			}
-		});
-		
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.X_AXIS));
-		buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		buttonPanel.add(Box.createGlue());
-		buttonPanel.add(ok);
-		
-		frame.add(label, BorderLayout.NORTH);
-		frame.add(scroll, BorderLayout.CENTER);
-		frame.add(buttonPanel, BorderLayout.SOUTH);
-		frame.pack();
-		frame.setSize(new Dimension(350,200));
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		if (owner == null || !owner.isVisible()) {
+			System.out.println(text);
+		} else {
+			final JDialog frame = new JDialog(owner,true);
+			frame.setTitle(title);
+			frame.setLayout(new BorderLayout());
+			//frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			
+			JLabel label = new JLabel(message,image, SwingConstants.LEFT);
+	
+			label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+			
+			JTextArea area = new JTextArea(text);
+			area.setEditable(false);
+			area.setWrapStyleWord(false);
+			area.setLineWrap(false);
+			
+			JScrollPane scroll = new JScrollPane(area);
+			scroll.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+			
+			JButton ok = new JButton("OK");
+			ok.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					frame.dispose();
+				}
+			});
+			
+			JPanel buttonPanel = new JPanel();
+			buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.X_AXIS));
+			buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+			buttonPanel.add(Box.createGlue());
+			buttonPanel.add(ok);
+			
+			frame.add(label, BorderLayout.NORTH);
+			frame.add(scroll, BorderLayout.CENTER);
+			frame.add(buttonPanel, BorderLayout.SOUTH);
+			frame.pack();
+			frame.setSize(new Dimension(350,200));
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+		}
 	}
 }
