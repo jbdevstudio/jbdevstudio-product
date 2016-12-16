30b72043afd5cb4b555f2dcff9198ac96f380936
Paul Leacu <pleacu@redhat.com>
2016-12-16 10:37:03 -0500

    JBTIS-948 - update runtime locations in a non-UI install
    JBTIS-948 - the file.createNewFile(); statement is unnecessary
    
    JBTIS-948 - move the runtime server semantics to the RuntimesPanelAutomationHelper class

--- installer/src/panels/com/jboss/devstudio/core/installer/InstallAdditionalRuntimesPanelAutomationHelper.java
+++ installer/src/panels/com/jboss/devstudio/core/installer/InstallAdditionalRuntimesPanelAutomationHelper.java
@@ -11,12 +11,19 @@
 
 package com.jboss.devstudio.core.installer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.izforge.izpack.adaptator.IXMLElement;
 import com.izforge.izpack.adaptator.impl.XMLElementImpl;
 import com.izforge.izpack.installer.AutomatedInstallData;
 import com.izforge.izpack.installer.InstallerException;
 import com.izforge.izpack.installer.PanelAutomation;
 import com.izforge.izpack.installer.Unpacker;
+import com.izforge.izpack.util.Debug;
+import com.jboss.devstudio.core.installer.bean.RuntimePath;
 
 /**
  * @author eskimo
@@ -32,11 +39,46 @@ public class InstallAdditionalRuntimesPanelAutomationHelper implements PanelAuto
 		panelRoot.addChild(locations);
 	}
 
+	/**
+	 * Establish the runtime_locations.properties file based on the JBoss Application Server location
+     * (if that panel is active) or minimally just the 'runtimes' directory.  This file is read at startup
+     * and affects the runtime UI preferences.  Also set the install data runtime locations variable.
+	 */
 	public void runAutomated(AutomatedInstallData idata, IXMLElement panelRoot) throws InstallerException {
 		IXMLElement locations = panelRoot.getFirstChildNamed(LOCATIONS_NODE_NAME);
 		if (locations != null && locations.getContent() != null) {
 			idata.setVariable(Unpacker.INSTALL_RT_LOCATIONS_VAR, locations.getContent());
 		}
-	}
+    	String installPath = idata.getVariable("INSTALL_PATH");
+		File folder = new File(installPath, P2DirectorStarterListener.DEVSTUDIO_LOCATION);
 
+		Properties servers = (Properties) idata.getAttribute("AS_SERVERS");
+		if (servers == null || servers.isEmpty()) {
+			servers = new Properties();
+			File runtimesFolder = new File (installPath, "runtimes");
+			RuntimePath runtimesPath = new RuntimePath(runtimesFolder.getAbsolutePath(), true);
+			servers.put("server1", runtimesPath.toString());
+		}
+		if (!folder.exists())
+			folder.mkdirs();
+
+		File file = new File(folder, "runtime_locations.properties");
+		FileOutputStream stream = null;
+		try {
+			stream = new FileOutputStream(file);
+			servers.store(stream, null);
+			stream.flush();
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (stream != null) {
+				try {
+					stream.close();
+				} catch (IOException e1) {
+					Debug.trace(e1);
+				}
+			}
+		}
+	}
 }
