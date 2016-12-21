3fc3a78b003bb0c3e88025d70850d52eb20b4c9b
nickboldt <nboldt@redhat.com>
2016-12-21 13:02:46 -0500

    JBDS-4230 JBDS-4204 add devstudio-wayland executable to work around wayland/x11 problems in Fedora 25
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- installer/src/panels/com/jboss/devstudio/core/installer/ShortcutPanelPatch.java
+++ installer/src/panels/com/jboss/devstudio/core/installer/ShortcutPanelPatch.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013-2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
 package com.jboss.devstudio.core.installer;
 
 import java.io.BufferedOutputStream;
@@ -43,6 +54,7 @@ public class ShortcutPanelPatch extends IzPanel {
 		"InstallConfigRecord.xml",
 		"devstudio",
 		"devstudio-unity",
+		"devstudio-wayland",
 		"JBoss-EULA.html",
 		"readme.txt"
 	};
